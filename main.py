a26b9dce638fd253fd7676cbece50d2ab6110462
Baboushka77 <157834490+Baboushka77@users.noreply.github.com>
2024-04-05 17:27:53 +0000

    Fonctionalité register + Rename dans le register

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-from flask import Flask, render_template, request, url_for, redirect, session 
+from flask import Flask, render_template, request, url_for, redirect, session
 
 import pymongo
 import os
-
+import bcrypt
 
 app = Flask("IVetCom")
 mongo = pymongo.MongoClient(os.getenv("MONGO_KEY"))
+app.secret_key = os.getenv("COOKIES_KEY")
+
 
 @app.route("/test")
 def test():
@@ -24,9 +26,44 @@ def index():
 def login():
   return render_template("login.html")
 
-@app.route("/register")
+@app.route('/register', methods=['POST', 'GET'])
 def register():
-  return render_template("register.html")
+    # Si on essaye de soumettre un formulaire
+    if request.method == 'POST':
+      # On verifie qu'un utilisateur du meme nom n'existe pas
+      db_utils = mongo.db.utilisateurs
+      # Si l'utilisateur existe déja on demande de re-remplir le formulaire
+      if (db_utils.find_one({'pseudo': request.form['pseudo']})):
+        return render_template('register.html',
+                               erreur="Ce pseudo existe deja")
+      # Sinon on créé l'utilisateur
+      else:
+        if (request.form['mot_de_passe'] == request.form['verif_mot_de_passe']):
+          # On crypte le mot de passe
+          mdp_encrypte = bcrypt.hashpw(
+            request.form['mot_de_passe'].encode('utf-8'), bcrypt.gensalt())
+          # On ajoute l'utilisateur
+          db_utils.insert_one({
+            'nom': request.form['nom'],
+            'prenom': request.form['prenom'],
+            'pseudo': request.form['pseudo'],
+            'mdp': mdp_encrypte,
+            'email': request.form['email'],
+            'numero_de_tel': request.form['numero_de_tel'],
+            'role': "abonné"
+          })
+          # On le connecte
+          session['util'] = request.form['pseudo']
+          # On retourne à la page d'accueil
+          return redirect(url_for('index'))
+        # Sinon on renvoie le template vide et met un message d'erreur
+        else:
+          return render_template(
+            'register.html', erreur="Les mots de passe doivent être identiques")
+    else:
+      return render_template('register.html')
+
+
 
 @app.route("/logout")
 def logout():
